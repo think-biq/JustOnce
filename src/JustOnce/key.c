cbe4b39fbcc8ea4956a1955b5bbab5871503c5fe
blurryroots <blurryroots@posteo.de>
2021-08-09 04:37:14 +0200

    import of normalize key to key module

--- src/JustOnce/key.c
+++ src/JustOnce/key.c
@@ -1,10 +1,48 @@
 
 #include "key.h"
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <baseencode/baseencode.h>
 #include "misc.h"
+
+void NormalizeKey(char** Key)
+{
+    const size_t RequiredKeyLength = 32;
+    const size_t KeyLength = strlen(*Key);
+    const int MissingKeyCharacters = RequiredKeyLength - KeyLength;
+
+    if (0 < MissingKeyCharacters)
+    {
+        char Appendix[MissingKeyCharacters];
+        for (size_t Index = 0; Index < MissingKeyCharacters; ++Index)
+        {
+            Appendix[Index] = '=';
+        }
+
+        char* NewKey = calloc(1, RequiredKeyLength + 1);
+        strcat(NewKey, *Key);
+        strcat(NewKey, Appendix);
+        NewKey[RequiredKeyLength] = '\0';
+
+        ToUpperCase(&NewKey);
+
+        free(*Key);
+        *Key = NewKey;
+    }
+    else if (0 > MissingKeyCharacters)
+    {        
+        char* NewKey = malloc(RequiredKeyLength + 1);
+        strncpy(NewKey, *Key, RequiredKeyLength);
+        NewKey[RequiredKeyLength] = '\0';
+
+        ToUpperCase(&NewKey);
+        
+        free(*Key);
+        *Key = NewKey;
+    }
+}
 
 char* GenerateSecretFromSeed(const char* Seed)
 {
