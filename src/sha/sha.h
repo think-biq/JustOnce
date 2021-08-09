41ad556464beb0eec036b167457b3634dae22876
blurryroots <blurryroots@posteo.de>
2021-08-09 07:07:14 +0200

    sha docs and discard of openssl includes

--- src/sha/sha.h
+++ src/sha/sha.h
@@ -8,12 +8,6 @@
 #ifndef SHA_H_
 #define SHA_H_ (1)
 
-/*#ifdef USE_OPENSSL
-#include <openssl/sha.h>
-#else*/
-
-/*  */
-
 /** SHA-1 Context */
 typedef struct {
     uint32_t state[5];
@@ -31,12 +25,28 @@ typedef SHA1_CTX SHA_CTX;
 /** SHA-1 Digest size in bytes (OpenSSL compat) */
 #define SHA_DIGEST_LENGTH SHA1_DIGEST_SIZE
 
+/**
+* Initialize new context
+*
+* @param context SHA1-Context
+*/
 void SHA1_Init(SHA1_CTX *context);
 
+/**
+* Run your data through this
+*
+* @param context SHA1-Context
+* @param p       Buffer to run SHA1 on
+* @param len     Number of bytes
+*/
 void SHA1_Update(SHA1_CTX *context, const void *p, size_t len);
 
+/**
+* Add padding and return the message digest
+*
+* @param digest  Generated message digest
+* @param context SHA1-Context
+*/
 void SHA1_Final(uint8_t digest[SHA1_DIGEST_SIZE], SHA1_CTX *context);
-
-/*#endif*/
 
 #endif // SHA_H_
